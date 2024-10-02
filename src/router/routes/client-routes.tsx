2e1cf6cfe70ef7f62d7c3a52780691f66a898648
dmytrenkoserhii <dmtrnk.srg@gmail.com>
2024-10-02 16:55:12 +0300

    feat: add notes layout and notes page

--- src/router/routes/client-routes.tsx
+++ src/router/routes/client-routes.tsx
@@ -1,21 +1,32 @@
 import { RouteObject } from 'react-router';
 import { AuthLayout, UnauthLayout } from '../../layouts';
 import Home from '../../pages/home';
+import { NotesLayout } from '../../features/notes/layouts/notes-layout';
+import { NotesPage } from '../../features/notes/pages/notes-page';
 
 export const CLIENT_ROUTES: RouteObject[] = [
   {
-    path: '/',
     element: <AuthLayout />,
     children: [
       {
-        index: true,
-        element: <Home />,
+        element: <NotesLayout />,
+        children: [
+          {
+            path: '/notes',
+            element: <NotesPage />,
+          },
+        ],
       },
     ],
   },
   {
-    path: '/auth',
     element: <UnauthLayout />,
-    children: [],
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <Home />,
+      },
+    ],
   },
 ];
