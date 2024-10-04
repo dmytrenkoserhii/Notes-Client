bfd56ab032402b0fbb2b787260494a7717c4b833
dmytrenkoserhii <dmtrnk.srg@gmail.com>
2024-10-04 15:18:40 +0300

    feat: add note item and note items list

--- src/features/notes/components/note-items-list.tsx
+++ src/features/notes/components/note-items-list.tsx
@@ -1,6 +1,25 @@
-import { Box } from '@mantine/core';
+import { Container, Stack } from '@mantine/core';
 import React from 'react';
+import { NoteItem } from './note-item';
+import { DUMMY_NOTES } from '../../../DUMMY_DATA';
+import { Note } from '../types';
 
 export const NoteItemsList: React.FC = () => {
-  return <Box>Note Items List</Box>;
+  const [notes, setNotes] = React.useState(DUMMY_NOTES.items);
+
+  const handleNoteUpdate = (updatedNote: Note) => {
+    setNotes(
+      notes.map((note) => (note.id === updatedNote.id ? updatedNote : note))
+    );
+  };
+
+  return (
+    <Stack mt={20}>
+      {notes.map((note) => (
+        <Container key={note.id}>
+          <NoteItem note={note} onUpdate={handleNoteUpdate} />
+        </Container>
+      ))}
+    </Stack>
+  );
 };
