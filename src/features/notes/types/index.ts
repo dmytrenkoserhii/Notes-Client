ca4a583b6b2434b4492fb5ef8e60179b91051bc6
dmytrenkoserhii <dmtrnk.srg@gmail.com>
2024-10-08 15:05:46 +0300

    feat: add interface for notes query params

--- src/features/notes/types/index.ts
+++ src/features/notes/types/index.ts
@@ -3,3 +3,4 @@ export type { CreateNoteRequestData } from './create-note-request-data.interface
 export type { EditNoteRequestData } from './edit-note-request-data.interface';
 export type { GetAllNotesRequestData } from './get-all-notes-request-data.interface';
 export type { GetAllNotesResponseData } from './get-all-notes-responce-data.interface';
+export type { NotesQueryParams } from './notes-query-params.interface';
